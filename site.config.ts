69434045286f76eaca6bc07c498de4a1b940b969
ayase <25816932+saki-ciallo@users.noreply.github.com>
2023-04-22 17:01:44 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -17,7 +17,7 @@ export default siteConfig({
   description: 'hypo meaning low',
 
   // social usernames (optional)
-  //twitter: 'null',
+  twitter: 'null',
   //github: 'null',
   //linkedin: 'null',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
